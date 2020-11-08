3816f4ecc652e7722a18a50e064990d541d2b426
fermi <fermiverse@gmail.com>
2020-11-09 00:08:21 +0530

    Added explicit ATLAS uri

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || "mongodb+srv://aalaapnair:geetanair@cluster0.ld8hs.mongodb.net/mixtape?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, function(error) {
     if (error) console.log("Error connecting: ", error);
 });
