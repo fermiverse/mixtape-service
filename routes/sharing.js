8a7c6a40dbb79a9c750da54d293c1a7714c978c2
fermi <fermiverse@gmail.com>
2020-11-16 23:45:23 +0530

    Added https config

---
+++ routes/sharing.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+let cors = require("cors");
+let User = require("../models/users.model");
+
+router.use(cors());
+
+router.route("/").get((req, res) => {
+    res.json({ping: 1});
+});
+
